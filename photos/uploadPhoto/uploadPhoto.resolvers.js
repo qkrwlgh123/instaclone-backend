0d5225a9920de078005d76ee686f3bf70e97efdb
qkrwlgh123 <jiho1024@naver.com>
2022-04-05 12:11:18 +0900

    Upload photo

---
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,40 @@
+import client from '../../client';
+import { protectedResolver } from '../../users/users.utils';
+
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
+        if (caption) {
+          // parse caption
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+          console.log(hashtagObj);
+          // get or create Hashtags
+          return client.photo.create({
+            data: {
+              file,
+              caption,
+              user: {
+                connect: {
+                  id: loggedInUser.id,
+                },
+              },
+              ...(hashtagObj.length > 0 && {
+                hashtags: {
+                  connectOrCreate: hashtagObj,
+                },
+              }),
+            },
+          });
+        }
+        // save the photo WITH the parsed hashtags
+        // add the photo to the hashtags
+      }
+    ),
+  },
+};
